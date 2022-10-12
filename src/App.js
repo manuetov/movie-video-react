b14a473cd93238f1622e9e87c2041787c92734c8
manuetov <manuetov@hotmail.com>
2022-10-12 12:53:58 +0200

    errors control done. MVP for technical test

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import YouTube from 'react-youtube';
 import './app.css'
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { Link } from 'react-router-dom'
 
 const App = () => {
   // https://developers.themoviedb.org/3/discover/movie-discover
@@ -22,6 +22,7 @@ const App = () => {
   const [selectedMovie, setSelectedMovie] = useState ({})
   const [playTrailer, setPlayTrailer] = useState(false)
 
+
   // all movies
   const getMovies = async (searchMovie) => {
     // solo si se hace una busqueda sino se muestra discover/movie
@@ -34,10 +35,14 @@ const App = () => {
     });
     // console.log(results);
     setIsFetching(false);
-    let randomResults = Math.floor(Math.random() * results.length);
-    selectMovie(results[randomResults])
+    let randomResults = Math.floor(Math.random() * results.length -1);
     setMovies(results);
+    selectMovie(results[randomResults])
   };
+  
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   // https://api.themoviedb.org/3/movie/157336?api_key={api_key}&append_to_response=videos
   // select on movie
@@ -57,9 +62,6 @@ const App = () => {
     setSelectedMovie(dataMovie)
   }
 
-  useEffect(() => {
-    getMovies();
-  }, []);
 
   // console.log(movies);
   if (isFetching) {
@@ -81,31 +83,37 @@ const App = () => {
     ));
   
   const renderTrailer = () => {
-    const trailer = selectedMovie.videos.results.find((eachVideo) => eachVideo.name === 'Official Trailer')
-    return  (
-      <YouTube 
-        videoId = {trailer.key} 
-        className = {'youtube-container'}
-        const opts = {{
-          width: '100%',
-          height: '100%',
-          playerVars: {
-            autoplay: 1
-          }
-        }}
-      />
-    )
-  }
+    try {
+      const trailer = selectedMovie.videos.results.find((eachVideo) => eachVideo.name === 'Official Trailer')
+      const key = trailer ? trailer.key : selectedMovie.videos.results[0].key
+      // console.log('key', key)
   
+      return  (
+         <YouTube 
+          videoId = {key} 
+          className = {'youtube-container'}
+          const opts = {{
+            width: '100%',
+            height: '100%',
+            playerVars: {
+              autoplay: 1
+            }
+          }}
+        /> 
+      ) 
+    } catch (error) {
+      console.log(error)   
+    }
+  }
 
   return (
     <Container fluid>
-      <h1 className=''>NETFLUXKIX</h1>
+      <h1 className='fw-bolder ' >NETFLUXKIX</h1>
       <Row className="d-flex justify-content-center">
         <header>
-        <div className="portada" style={{backgroundImage: `url('${IMAGE_PATH}${selectedMovie.backdrop_path}')`}}>
+          <div className="portada" style={{backgroundImage: `url('${IMAGE_PATH}${selectedMovie.backdrop_path}')`}}>
             <div className="portada-content">
-              {playTrailer ? <Button variant='dark'
+              {playTrailer ? <Button variant='danger'
                 className="position-absolute top-0 start-50 translate-middle"
                 style={{zIndex: '100'}}  
                 onClick={() => setPlayTrailer(false)}>Cerrar</Button> : null}
@@ -116,25 +124,25 @@ const App = () => {
               {selectedMovie.overview ? <p>{selectedMovie.overview}</p> : null}
             </div>
           </div>
-          {/* <Form */}
         </header>
+          {/* <Form */}
           <form onSubmit={searchMoviesSubmit}>
               <Form.Control 
-                className="h-50 d-inline-block m-3 text-info"
+                className="h-50 d-inline-block m-3 text-white"
                 style={{ width: 500, backgroundColor: "rgba(0, 0, 255, 0.1)" }}
                 type="text"
-                placeholder="Buscar peli"
+                placeholder="Indroduce una palabra de la película que quieres buscar"
                 onChange={(e) => setSearchMovie(e.target.value)}
               />
               <Button
                 style={{ width: 120 }}
-                variant="primary"
+                variant="outline-primary"
+                size='sm'
                 type="submit"
               >
                 Buscar
               </Button>
-          </form>
-          
+          </form>    
         {renderMovies()}
       </Row>
     </Container>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-const MovieCard = ({ movie, selectMovie}) => {
-//   console.log(movie);
+const MovieCard = ({ movie, selectMovie }) => {
+  //   console.log(movie);
   // https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
   const img_URL = "https://image.tmdb.org/t/p/w500";
 
-  return (
-    
-      <Card className='card m-3' 
-         style={{ width: "18rem" }} 
-         onClick={() => selectMovie(movie)}
-      >
-        {movie.poster_path ? (
-          <Card.Img src={`${img_URL}${movie.poster_path}`} alt="Card image" />
-        ) : null}
-        <Card.Body>
-         <Card.Title className='p-2 fs-3 fw-bold text-danger'>{movie.title}</Card.Title>
-         <Card.Text>{movie.overview}</Card.Text>
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
-        </Card.Body>
-      </Card>
-    
+  return (
+    <Card
+      className="card bg-dark m-2"
+      style={{ width: "18rem" }}
+      onClick={() => selectMovie(movie)}
+    >
+      {movie.poster_path ? (
+        <Card.Img src={`${img_URL}${movie.poster_path}`} alt="Card image" />
+      ) : null}
+      <Card.Body>
+        <Card.Title className="p-2 fs-3 fw-bold text-danger">
+          {movie.title}
+        </Card.Title>
+        <Card.Text>{truncate(movie?.overview, 100)}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
